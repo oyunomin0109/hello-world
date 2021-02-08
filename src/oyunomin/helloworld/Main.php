98526846c6fa155436a50248b1cb93f6fd300487
oyunomin0109 <68724268+oyunomin0109@users.noreply.github.com>
2021-02-08 22:36:00 +0900

    Update Main.php

--- src/oyunomin/helloworld/Main.php
+++ src/oyunomin/helloworld/Main.php
@@ -1,13 +1,14 @@
 <?php
 
-namespace oyunomin\helloworld;
-
-use pocketmine\{Player,Server};
+namespace oyunomin\helloworld; //このファイルのパスを書くよ
+//使うもののパスを書くよ
+use pocketmine\{Player,Server}; //同じパスのものは{ファイル名,ファイル名};と短縮できるよ
 use pocketmine\event\Listener;
-use pocketmine\plugin\PluginBase;
+use pocketmine\plugin\PluginBase; 
+//今回必要なuse文だよ
 
 class Main extends PluginBase implements Listener{
-    public function onEnable(){
-        $this->getServer()->getLogger()->info("helloworld");
+    public function onEnable(){ //この関数はサーバーが起動時になったときに呼び出されるよ
+        $this->getServer()->getLogger()->info("helloworld"); //このpluginが読み込まれた時にコンソールにhelloworldと表示させるよ)
     }
 }
